90fcd3a9eb31d233bffc3792cb3fa77580364973
dima <telehadmytro@gmail.com>
2024-07-21 22:25:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import video from './am-so.mp4'
+
 
 function App() {
+
+  const [getMyAdvice, setGetMyAdvice] = useState("")
+
+  useEffect(() => {
+getNewAdvice();
+}, [])
+
+  
+  
+  const getNewAdvice = async () => {
+  const response = await fetch(`https://bored.api.lewagon.com/api/activity`)
+  const data = await response.json()  
+  console.log(data.activity) 
+  setGetMyAdvice(data.activity)  
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div className="container">
+    <video autoPlay muted loop>
+    <source src={video} type="video/mp4" />
+    </video>
+        <div className='advice'>
+        <p><b>{getMyAdvice}</b></p>  
+        <button onClick={getNewAdvice}><img src='https://img.icons8.com/?size=100&id=GdswgxgvDo0a&format=png&color=399918' height='30px'alt='pict'/>New Advice<img src='https://img.icons8.com/?size=100&id=GdswgxgvDo0a&format=png&color=399918' height='30px'alt='pict'/></button>
+        </div>
+    </div> 
+    </div> 
+  
   );
 }
 
